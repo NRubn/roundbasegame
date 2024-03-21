b4b453bc411f597c45214325d35925d044b0c198
NRubn <norgallruben@gmail.com>
2024-03-21 21:40:31 +0100

    KI fix gamecontroller

--- resources/js/gamecontroller.js
+++ resources/js/gamecontroller.js
@@ -8,19 +8,15 @@ class GameController {
         this.displayCurrentCharacter = displayCurrentCharacter; // Methode zur Anzeige des aktuellen Charakters als Parameter übergeben
     }
 
-    // Andere Methoden wie addCharacter(), startNewRound(), getNextCharacter(), getCurrentCharacter(), getAllCharacterCoordinates(), getTotalActionPoints() bleiben unverändert...
-    // Methode zum Hinzufügen eines Charakters zum Spiel
     addCharacter(character) {
         this.characters.push(character);
     }
 
-    startGame(){
+    startGame() {
         this.drawGrid();
     };
 
-    // Methode zum Starten einer neuen Runde
     startNewRound() {
-        
         this.roundNumber++;
         this.currentCharacterIndex = 0; // Setze den Index des aktuellen Charakters zurück
         this.displayCurrentCharacterStats();
@@ -28,41 +24,35 @@ class GameController {
             character.resetActionPoints();
         });
     }
-    // Methode zur Bestimmung des nächsten Charakters, der ziehen darf
+
     getNextCharacter() {
         const nextCharacter = this.characters[this.currentCharacterIndex];
         this.currentCharacterIndex = (this.currentCharacterIndex + 1) % this.characters.length;
         this.displayCurrentCharacterStats();
+        this.drawGrid();
         return nextCharacter;
     }
-    // Methode zur Rückgabe des aktuellen Charakters
+
     getCurrentCharacter() {
         return this.characters[this.currentCharacterIndex];
     }
-    // Methode zur Rückgabe aller Koordinaten der Charaktere
+
     getAllCharacterCoordinates() {
-        const occupiedCoordinates = [];
-        this.characters.forEach(character => {
-            occupiedCoordinates.push(character.position);
-        });
-        return occupiedCoordinates;
+        return this.characters.map(character => character.position);
     }
-    // Methode zur Berechnung der Gesamtaktionen aller Charaktere
+
     getTotalActionPoints() {
-        let totalActionPoints = 0;
-        this.characters.forEach(character => {
-            totalActionPoints += character.actionPoints;
-        });
-        return totalActionPoints;
+        return this.characters.reduce((total, character) => total + character.actionPoints, 0);
     }
-    
-    // Funktion zum Zeichnen des Gridmusters und der Charaktere
+
     drawGrid() {
         const ctx = this.field.ctx;
         const cellWidth = this.field.cellWidth;
         const cellHeight = this.field.cellHeight;
         const activeCharacter = this.getCurrentCharacter(); // Aktuellen Charakter abrufen
-    
+
+        ctx.clearRect(0, 0, this.field.width, this.field.height);
+
         for (let x = 0; x < this.field.xfields; x++) {
             for (let y = 0; y < this.field.yfields; y++) {
                 ctx.beginPath();
@@ -74,7 +64,7 @@ class GameController {
                 ctx.stroke();
             }
         }
-    
+
         this.characters.forEach(character => {
             ctx.fillStyle = character.color;
             ctx.fillRect(character.position[0] * cellWidth, character.position[1] * cellHeight, cellWidth, cellHeight);
@@ -82,20 +72,18 @@ class GameController {
                 ctx.drawImage(character.heroImg, character.position[0] * cellWidth, character.position[1] * cellHeight, cellWidth, cellHeight);
             }
         });
-    
+
         // Überprüfen, ob der aktive Charakter vorhanden ist
         if (activeCharacter) {
             ctx.strokeStyle = 'yellow'; // Gelber Rand
             ctx.lineWidth = 3; // Dicke des Randes
             ctx.strokeRect(activeCharacter.position[0] * cellWidth, activeCharacter.position[1] * cellHeight, cellWidth, cellHeight);
         }
-    
+
         this.roundNumberDisplay.textContent = this.roundNumber;
         this.field.drawObstacles();
     }
-    
 
-    // Methode zur Überprüfung, ob das Zielfeld gültig ist
     isValidMove(newX, newY) {
         // Überprüfen, ob das Zielfeld innerhalb der Grenzen des Spielfelds liegt
         if (newX >= 0 && newX < this.field.xfields && newY >= 0 && newY < this.field.yfields) {
@@ -117,7 +105,7 @@ class GameController {
         }
         return false; // Zielfeld liegt außerhalb des Spielfelds
     }
-    // Funktion zum Bewegen des Charakters
+
     moveHero(character, deltaX, deltaY) {
         const newHeroX = character.position[0] + deltaX;
         const newHeroY = character.position[1] + deltaY;
@@ -129,13 +117,11 @@ class GameController {
 
             character.actionPoints--;
 
-            //actionPointsDisplay.textContent = character.actionPoints;
-
-
-            if(this.getTotalActionPoints() === 0){
+            if (this.getTotalActionPoints() === 0) {
                 this.startNewRound();
             }
-
+            
+            this.displayCurrentCharacterStats();
             this.field.clearCanvas();
             this.drawGrid();
         }
@@ -144,22 +130,22 @@ class GameController {
     displayCurrentCharacterStats() {
         const currentCharacterStats = this.getCurrentCharacter();
 
-        console.log(currentCharacterStats);
-
         let actionsHTML = '<div id="actions">';
 
-        currentCharacterStats.actions.forEach(action => {
-            actionsHTML += `
-                <div class="${action}" data-type="${action}"><button class="button">${action}</button></div>
-            `;
-        });
+        if (currentCharacterStats.actions) { // Überprüfen, ob actions definiert ist
+            currentCharacterStats.actions.forEach(action => {
+                actionsHTML += `
+                    <div class="${action}" data-type="${action}"><button class="button">${action}</button></div>
+                `;
+            });
+        }
 
         actionsHTML += '</div>';
         // Erstellen des HTML-Gerüsts für den aktuellen Charakter
         const characterStatsHTML = `
             <div id="activehero">${currentCharacterStats.name}</div>
             <div class="stats">
-                <div id="actionpoints">AP ${currentCharacterStats.actionPoints}</div>
+                <div id="actionpoints">AP ${currentCharacterStats.actionPoints}</
                 <div id="hp"> HP ${currentCharacterStats.hp}</div>
                 <div id="xp"> XP ${currentCharacterStats.xp}</div>
                 <div id="attack"> AT ${currentCharacterStats.attack}</div>
@@ -169,7 +155,6 @@ class GameController {
         `;
 
         // Elemente einfügen
-        this.displayCurrentCharacter.innerHTML = characterStatsHTML;
-        this.drawGrid();
+        document.getElementById("activeherostats").innerHTML = characterStatsHTML;
     }
 }
