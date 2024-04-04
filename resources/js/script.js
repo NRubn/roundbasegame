f27b9c5ce3178354a5197ada68bd5860004d6df8
NRubn <norgallruben@gmail.com>
2024-04-04 13:32:32 +0200

    fix few saves

--- resources/js/script.js
+++ resources/js/script.js
@@ -13,7 +13,7 @@ const gameController = new GameController(roundNumberDisplay,activeherostatsCont
 const hero1 = new Character("Green Hero 1", 3, 3, 24, 0, 5, 3,"green", "resources/char/hero.svg",'player1');
 const hero2 = new Character("Red Hero 2", 1, 1, 19, 0, 4, 4,"red", "resources/char/person-hiking-solid.svg",'player1');
 const hero3 = new Character("Blue Bob", 1, 2, 20, 0, 4, 4,"blue", "resources/char/hero.svg",'player1');
-const enemy1 = new Character("Blue Bob", 7, 7, 20, 0, 4, 4,"white", "resources/char/spider-solid.svg",'enemy');
+const enemy1 = new Character("Spider", 7, 7, 20, 0, 4, 4,"white", "resources/char/spider-solid.svg",'enemy');
 const thisplayerteam = 'player1';
 
 hero3.addAction('shoot');
